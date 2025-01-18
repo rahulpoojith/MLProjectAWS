0e5330bf230705349eeeaea5c7cd20fce2bef6a9
rahulpoojith <poojith.p.rahul@gmail.com>
2025-01-18 12:31:56 +0530

    new file

--- application.py
+++ application.py
@@ -36,4 +36,4 @@ def predict_datapoint():
             return render_template('home.html', error=str(e))
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5001, debug=False)
